6651f9175b720d04f03f5dd684393970aa8fc3b9
tran-drew <30425426+tran-drew@users.noreply.github.com>
2017-07-27 23:22:18 -0700

    Update imageScraper.go

--- imagescraper/imageScraper.go
+++ imagescraper/imageScraper.go
@@ -1,57 +1,81 @@
 package main
-
 import (
 	"net/http"
-	// "io/ioutil"
+	"bufio"
 	"image/png"
 	"image/jpeg"
 	"os"
-	"fmt"
 	"strings"
+	"fmt"
 )
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func getPNGImage(url []string, name string) {
 
 	resp, err := http.Get(strings.Join(url, "/"))
-	if err != nil {
-			fmt.Println(err)
-	}
+	check(err)
 	defer resp.Body.Close()
 	// bytes, _ := ioutil.ReadAll(resp.Body)
 
 	img, err := png.Decode(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
+	check(err)
 
 	filename := "./images/" + name
 	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0777)
-	if err != nil {
-		fmt.Println(err)
-	}
+	check(err)
+	
 	png.Encode(f, img)
 	return
 }
 
+func readLines(path string) ([]string, error) {
+  file, err := os.Open(path)
+  if err != nil {
+    return nil, err
+  }
+  defer file.Close()
+
+  var lines []string
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    lines = append(lines, scanner.Text())
+  }
+  return lines, scanner.Err()
+}
+
+// writeLines writes the lines to the given file.
+func writeLines(lines []string, path string) error {
+  file, err := os.Create(path)
+  if err != nil {
+    return err
+  }
+  defer file.Close()
+
+  w := bufio.NewWriter(file)
+  for _, line := range lines {
+    fmt.Fprintln(w, line)
+  }
+  return w.Flush()
+}
 func getJPGImage(url []string, name string) {
 	resp, err := http.Get(strings.Join(url, "/"))
-	if err != nil {
-			fmt.Println(err)
-	}
+	check(err)
 	defer resp.Body.Close()
 
 	img, err := jpeg.Decode(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
+	check(err)
 
-	filename := "../images/" + name
+	filename := "./images/" + name
 	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0777)
-	if err != nil {
-		fmt.Println(err)
-	}
-	o := jpeg.Options{Quality: 100}
-	jpeg.Encode(f, img, *o)
+	check(err)
+
+	
+	jpeg.Encode(f, img, nil)
 	return
 }
 
@@ -63,13 +87,19 @@ func getJPGImage(url []string, name string) {
 
 func main() {
 
-	url := strings.Split("http://www.parkavenueorthodonticsnyc.com/images/slide3.jpg", "/")
-
-	switch {
-	case strings.Contains(url[len(url)-1], ".png"):
-		getPNGImage(url, url[len(url)-1])
-	case strings.Contains(url[len(url)-1], ".jpg"):
-		getJPGImage(url, url[len(url)-1])
+	lines, err := readLines("foo.in.txt")
+	check(err)
+	for i, line := range lines {
+		url := strings.Split(line, "/")
+		fmt.Println(i, line)
+			switch {
+				case strings.Contains(url[len(url)-1], ".png"):
+					getPNGImage(url, url[len(url)-1])
+				case strings.Contains(url[len(url)-1], ".jpg"):
+					getJPGImage(url, url[len(url)-1])
+				case strings.Contains(url[len(url)-1], ".jpeg"):
+					getJPGImage(url, url[len(url)-1])
+			}
 	}
 	
 }
