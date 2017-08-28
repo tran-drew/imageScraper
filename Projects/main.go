de4e379fab25e7a9b6c73cec63cc1cd2013aa453
tran-drew <a.tran.fftc+github@gmail.com>
2017-08-28 12:42:13 -0700

    csv reading/writing

--- Projects/main.go
+++ Projects/main.go
@@ -1,16 +1,48 @@
 package main
 
 import (
-    "io"
-    "net/http"
+	"bufio"
+	"encoding/csv"
+	"os"
+	"fmt"
+	"io"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-    io.WriteString(w, "Hello world!")
+func main() {
+	f, _ := os.Open("Accounts.csv")
+
+    // Create a new reader.
+    r := csv.NewReader(bufio.NewReader(f))
+
+    file, err := os.Create("New.csv")
+    checkError("Cannot create file", err)
+    defer file.Close()
+
+    writer := csv.NewWriter(file)
+    defer writer.Flush()
+
+    for {
+        record, err := r.Read()
+        // Stop at EOF.
+        if err == io.EOF {
+            break
+        }
+        // Display record.
+        // ... Display record length.
+        // ... Display all individual elements of the slice.
+        fmt.Println(record)
+        fmt.Println(len(record))
+        errors := writer.Write(record)
+        checkError("Cannot write to file", errors)
+        for value := range record {
+            fmt.Printf("  %v\n", record[value])
+        }
+    }
+
 }
 
-func main() {
-    mux := http.NewServeMux()
-    mux.HandleFunc("/", hello)
-    http.ListenAndServe(":8000", mux)
+func checkError(message string, err error) {
+    if err != nil {
+        panic(err)
+    }
 }
